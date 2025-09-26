032b3a2bd877ebd1ff1e5fa10547c78cd32c307b
pablogarciastc <pgsantaclara@gradiant.org>
2025-09-26 12:00:09 +0930

    Backward prop changes

--- engine_leapgen.py
+++ engine_leapgen.py
@@ -218,7 +218,7 @@ def train_one_epoch_with_aux(
             if "reduce_sim2" in output2 and output2["reduce_sim2"] is not None:
                 loss2 = -1 * args.pull_constraint_coeff2 * output2['reduce_sim2']
                 task_optimizer.zero_grad()
-                loss2.backward()
+                loss2.backward(retain_graph=True)
                 task_optimizer.step()
 
         if device.type == 'cuda':
